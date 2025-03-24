2d7e46a9c98e72622f667f653f16df9142fbc0a8
Kim-Byeong-Hun <119733220+Kim-Byeong-Hun@users.noreply.github.com>
2025-03-24 12:14:16 +0000

    Deploying to gh-pages from @ Kim-Byeong-Hun/Kim-Byeong-Hun.github.io@135e04ff63f1f760661661c4d993b0224fb9182b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "",
+          description: "AI 기반 개인 연구 및 팀 프로젝트들을 소개합니다. 각 프로젝트는 기술적 배경, 구현 방식, 실험 결과 등을 포함하고 있습니다.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -163,51 +163,26 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
+            },},{id: "projects-vision-기반-부모-아동-상호작용-측정-시스템-개발",
+          title: 'Vision 기반 부모-아동 상호작용 측정 시스템 개발',
+          description: "감성 지능형 아동케어시스템을 위한 비전 기반 부모-아동 상호작용 모듈 개발",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
+            },},{id: "projects-project-1",
+          title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/4_project/";
             },},{
         id: 'social-email',
         title: 'email',
